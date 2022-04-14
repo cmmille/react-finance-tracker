27c23d7c62f1713002015f1912f5638bc0200ce7
cmmille <cmmiller.bio@gmail.com>
2022-04-13 23:34:23 -0600

    Added chart graphic.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
-import { useState } from "react"
+import { useState } from "react";
 import ExpenseList from "./components/Expenses/ExpenseList";
 import NewExpense from "./components/NewExpense/NewExpense";
 import { uid } from "uid";
 
 const App = () => {
   const initialExpenses = [
-    {title: "King Soopers", date: new Date(2021, 2, 10), amount: 96.28, key: uid()},
-    {title: "Liquor Store", date: new Date(2019, 2, 8), amount: 19.99, key: uid()},
-    {title: "Amazon", date: new Date(2022, 1, 28), amount: 96.28, key: uid()},
-    {title: "Microsoft", date: new Date(2022, 2, 1), amount: 96.28, key: uid()}
+    {
+      title: "King Soopers",
+      date: new Date(2021, 2, 10),
+      amount: 96.28,
+      key: uid(),
+    },
+    {
+      title: "Liquor Store",
+      date: new Date(2019, 2, 8),
+      amount: 19.99,
+      key: uid(),
+    },
+    { title: "Amazon", date: new Date(2022, 1, 28), amount: 120.4, key: uid() },
+    {
+      title: "Microsoft",
+      date: new Date(2022, 2, 1),
+      amount: 59.99,
+      key: uid(),
+    },
   ];
-  const [expenses, setExpenses] = useState(initialExpenses)
+  const [expenses, setExpenses] = useState(initialExpenses);
 
-  function addExpenseHandler(expenseItem){
-    const newExpense = {...expenseItem, date: new Date(`${expenseItem.date} `), key: uid()}
-    setExpenses(prevState => [newExpense, ...prevState])
+  function addExpenseHandler(expenseItem) {
+    const newExpense = {
+      ...expenseItem,
+      date: new Date(`${expenseItem.date} `),
+      amount: parseFloat(expenseItem.amount),
+      key: uid(),
+    };
+    setExpenses((prevState) => [newExpense, ...prevState]);
   }
-
 
   return (
     <div>
-      <NewExpense onAddExpense = {addExpenseHandler}/>
-      <ExpenseList expenses = {expenses}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <ExpenseList expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -4,31 +4,37 @@ import "./ExpenseList.css";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 
 const ExpenseList = (props) => {
-  
-  const expenses = props.expenses.sort((a,b)=>(a.date < b.date) ? 1 : -1);
-  const allYears = [...new Set(expenses.map(expense => expense.date.getFullYear()))].sort().reverse()
+  const expenses = props.expenses.sort((a, b) => (a.date < b.date ? 1 : -1));
+  const allYears = [
+    ...new Set(expenses.map((expense) => expense.date.getFullYear())),
+  ]
+    .sort()
+    .reverse();
   const [filterYear, setFilterYear] = useState(-1);
 
   const filteredExpenses = expenses.filter((expense) => {
     return expense.date.getFullYear() === filterYear;
   });
-  
+
   function filterHandler(filterValue) {
     setFilterYear(parseInt(filterValue));
   }
-  
+
+  const passedExpenses = (filterYear === -1 ? expenses : filteredExpenses)
 
   return (
     <div>
       <Card className="expense-list">
-        <ExpensesFilter 
-        onFilter={filterHandler} 
-        selectedYear = {filterYear}
-        filterOptions = {allYears}
+        <ExpensesFilter
+          onFilter={filterHandler}
+          selectedYear={filterYear}
+          filterOptions={allYears}
         />
-        {(filterYear === -1 ? expenses : filteredExpenses).map((expense) => {
+        <ExpensesChart expenses={passedExpenses} />
+        {passedExpenses.map((expense) => {
           return (
             <ExpenseItem
               key={expense.key}
